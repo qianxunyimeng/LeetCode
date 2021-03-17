783a4f85aa91dfd44649b3f88757c8f9eb3d6be3
qianxunyimeng <460960620@qq.com>
2021-03-18 00:11:25 +0800

    X的平方根

--- src/main/java/com/qianxun/easy/Test_0069X的平方根.java
+++ src/main/java/com/qianxun/easy/Test_0069X的平方根.java
@@ -3,7 +3,7 @@ package com.qianxun.easy;
 public class Test_0069X的平方根 {
 
 	public static void main(String[] args) {
-		System.out.println("9 的平方根 = " + mySqrt(9) );
+		System.out.println("9 的平方根 = " + mySqrt(2147395599) );
 		//System.out.println("12的平方根 = " + mySqrt(12) );
 	}
 	
@@ -15,12 +15,12 @@ public class Test_0069X的平方根 {
 	 */
 	public static int mySqrt(int x) {
 		int low = 1;
-		int high = x - 1;
-		int ans = -1;
+		int high = x;
+		int ans = 0;
 		
 		while(low <= high) {
 			int mid = low + ((high - low) >> 1);
-		    if( mid * mid <= x) {
+		    if( (long)mid * mid <= x) {
 		    	ans = mid;
 		    	low = mid + 1;
 		    }else {
